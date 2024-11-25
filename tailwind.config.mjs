e8d40d7e70913ae36b72c1950dcb3dba7080778d
Brandon Hernández <isahecam@gmail.com>
2024-11-24 22:37:10 -0600

    Add astro-icon integration and update Tailwind CSS configuration with custom theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-cover": "url('/hero-cover.png')",
+      },
+      colors: {
+        primary: "#FF7B47",
+        secondary: "#0D5C63",
+        "gray-soft": "#737373",
+        normal: "#252B42",
+      },
+      fontFamily: {
+        sans: ["Montserrat Variable", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
